6def3920c48339fe0a3389bff8cf8c7655f047e9
cadirnberger <cadirnberger@willamette.edu>
2025-09-30 13:04:38 -0700

    fixed the ladder

--- FoxParkour/2dplatformer/Assets/climb.cs
+++ FoxParkour/2dplatformer/Assets/climb.cs
@@ -4,46 +4,32 @@ using UnityEngine.InputSystem;
 public class Climb : MonoBehaviour
 {
     public float climbSpeed = 5f;
-    public static bool isClimbing = false;
+    public static bool isClimbing;
+    public static bool isLadder;
+    public static bool isPlatform;
     private float vertical;
-    private Rigidbody2D rb;
-    
-   
-    
+    [SerializeField]private Rigidbody2D rb;
 
-    void Start()
-    {
-        rb = GetComponent<Rigidbody2D>();
-        rb.freezeRotation = true;
-    }
 
     void Update()
     {
-        if (isClimbing)
+        vertical = Input.GetAxisRaw("Vertical"); // W/S or Up/Down
+        if (isLadder && Mathf.Abs(vertical) > 0 && isPlatform)
         {
-            vertical = Input.GetAxisRaw("Vertical"); // W/S or Up/Down
+            isClimbing = true;
         }
-
-        else
-        {
-            vertical = 0f;
-
-
-        }
+        
     }
-
-    void FixedUpdate()
+    private void FixedUpdate()
     {
         if (isClimbing)
         {
-            rb.gravityScale = 0f; // disable gravity while climbing
             rb.linearVelocity = new Vector2(rb.linearVelocity.x, vertical * climbSpeed);
+            rb.gravityScale = 0f; // Disable gravity while climbing
         }
         else
         {
-            rb.gravityScale = 1f; // restore gravity
-
-
+            rb.gravityScale = 1f; // Enable gravity when not climbing
         }
     }
 
@@ -52,8 +38,13 @@ public class Climb : MonoBehaviour
         if (collision.CompareTag("Ladder"))
         {
             isClimbing = true;
-            
-            
+            gameObject.layer = LayerMask.NameToLayer("PlayerClimbing");
+
+
+        }
+        if (collision.CompareTag("Platform"))
+        {
+            isPlatform = true;
         }
     }
 
@@ -62,7 +53,11 @@ public class Climb : MonoBehaviour
         if (collision.CompareTag("Ladder"))
         {
             isClimbing = false;
-            
+            isLadder = false;
+            gameObject.layer = LayerMask.NameToLayer("PlayerNormal");
+
+
         }
+        
     }
 }
